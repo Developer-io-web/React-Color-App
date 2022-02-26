5eb77d4e27b57cc48feeb748ad60950280b3734b
Jayant <jayant2@s.amity.edu>
2022-02-26 22:50:30 +0530

    Ready To Deploy

--- src/seedColor.js
+++ src/seedColor.js
@@ -1,8 +1,8 @@
-const color= [
+const seedClr= [
     {
       paletteName: "Material UI Colors",
       id: "material-ui-colors",
-      emoji: "🎨",
+      emoji: ["🎨","☺️","🤗","🤩","🤔","🤨","🧐","🤓","🤯","🤪","🤫","🤬","🤭","🤮","🤯"],
       colors: [
         { name: "red", color: "#F44336" },
         { name: "pink", color: "#E91E63" },
@@ -28,7 +28,7 @@ const color= [
     {
       paletteName: "Flat UI Colors v1",
       id: "flat-ui-colors-v1",
-      emoji: "🤙",
+      emoji: ["🤙","🤠","😎","😅","😂","😇","😍","😘","😜","😝","😋","😎","😭","😡","😢","😣"],
       colors: [
         { name: "Turquoise", color: "#1abc9c" },
         { name: "Emerald", color: "#2ecc71" },
@@ -55,7 +55,7 @@ const color= [
     {
       paletteName: "Flat UI Colors Dutch",
       id: "flat-ui-colors-dutch",
-      emoji: "🇳🇱",
+      emoji: ["🇳🇱","🍀","🌈","🌊","🌋","🌌","🌏","🌑","🌓","🌔","🌕","🌙","🌛","🌟","🌠","🌤","🌧","🌨"],
       colors: [
         { name: "Sunflower", color: "#FFC312" },
         { name: "Energos", color: "#C4E538" },
@@ -82,7 +82,7 @@ const color= [
     {
       paletteName: "Flat UI Colors American",
       id: "flat-ui-colors-american",
-      emoji: "🇺🇸",
+      emoji: ["🇺🇸","🎄","🌳","⛲","🎢","🚢","🎡","🎠","🏗","🏭","📦","📥","📱","📲","💻","💾","💽"],
       colors: [
         { name: "LightGreenishBlue", color: "#55efc4" },
         { name: "FadedPoster", color: "#81ecec" },
@@ -109,7 +109,7 @@ const color= [
     {
       paletteName: "Flat UI Colors Aussie",
       id: "flat-ui-colors-aussie",
-      emoji: "🇦🇺",
+      emoji: ["🇦🇺","🎅","🎄","🌳","⛲","🎢","🚢","🎡","🏗","🏭","📦","📥","📱","📲","💻","💾","💽"],
       colors: [
         { name: "Beekeeper", color: "#f6e58d" },
         { name: "SpicedNectarine", color: "#ffbe76" },
@@ -136,7 +136,7 @@ const color= [
     {
       paletteName: "Flat UI Colors British",
       id: "flat-ui-colors-british",
-      emoji: "🇬🇧",
+      emoji: ["🇬🇧","🗽","🌚","📰","📚","📕","📗","📘","📙","📓","📒","📂","🗞","📰","📚","📕","📗","📘","📙","📓","📒","📂","🗞"],
       colors: [
         { name: "ProtossPylon", color: "#00a8ff" },
         { name: "Periwinkle", color: "#9c88ff" },
@@ -163,7 +163,7 @@ const color= [
     {
       paletteName: "Flat UI Colors Spanish",
       id: "flat-ui-colors-spanish",
-      emoji: "🇪🇸",
+      emoji: ["🇪🇸","🎁","🎀","🎉","🎊","🎈","🎓","🎏","🎀","🎁","🎉","🎊","🎈","🎓","🎏"],
       colors: [
         { name: "JacksonsPurple", color: "#40407a" },
         { name: "C64Purple", color: "#706fd3" },
@@ -190,7 +190,7 @@ const color= [
     {
       paletteName: "Flat UI Colors Indian",
       id: "flat-ui-colors-indian",
-      emoji: "🇮🇳",
+      emoji: ["🇮🇳","🍕","🍔","🍟","🍝","🍛","🍤","🍢","🍣","🍱","🍛","🍤","🍢","🍣","🍱"],
       colors: [
         { name: "OrchidOrange", color: "#FEA47F" },
         { name: "SpiroDiscoBall", color: "#25CCF7" },
@@ -217,7 +217,7 @@ const color= [
     {
       paletteName: "Flat UI Colors French",
       id: "flat-ui-colors-french",
-      emoji: "🇫🇷",
+      emoji: ["🇫🇷","🍟","🥖","🥐","🧇","🍔","🍟","🥖","🥐","🧇","🍔"],
       colors: [
         { name: "FlatFlesh", color: "#fad390" },
         { name: "MelonMelody", color: "#f8c291" },
@@ -243,4 +243,4 @@ const color= [
     }
   ];
 
-  export default color;
+  export default seedClr;
